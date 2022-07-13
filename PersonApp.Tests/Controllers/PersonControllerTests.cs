022dbbdb14c67ade157771738c82016879b9b9cb
iamfranco <kinfaifrancochan@gmail.com>
2022-07-13 22:51:53 +0100

    add unit test for PersonController constructors, add PersonController class, blank IPersonContext interface, and blank IPersonCsvParser interface to pass test

---
+++ PersonApp.Tests/Controllers/PersonControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using PersonApp.Contexts;
+using PersonApp.Controllers;
+using PersonApp.CsvParsers;
+
+namespace PersonApp.Tests.Controllers;
+internal class PersonControllerTests
+{
+    private Mock<IPersonContext> _mockPersonContext;
+    private Mock<IPersonCsvParser> _mockPersonCsvParser;
+    private PersonController _personController;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockPersonContext = new Mock<IPersonContext>();
+        _mockPersonCsvParser = new Mock<IPersonCsvParser>();
+
+        _personController = new PersonController(_mockPersonContext.Object, _mockPersonCsvParser.Object);
+    }
+
+    [Test]
+    public void Constructor_With_Null_PersonContext_Should_Throw_Null_Exception()
+    {
+        Action act = () => _personController = new PersonController(null, _mockPersonCsvParser.Object);
+
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("personContext should not be null");
+    }
+
+    [Test]
+    public void Constructor_With_Null_PersonCsvParser_Should_Throw_Null_Exception()
+    {
+        Action act = () => _personController = new PersonController(_mockPersonContext.Object, null);
+
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("personCsvParser should not be null");
+    }
+
+}
---
+++ PersonApp/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using PersonApp.Contexts;
+using PersonApp.CsvParsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp.Controllers;
+public class PersonController
+{
+    private IPersonContext _personContext;
+    private IPersonCsvParser _personCsvParser;
+
+    public PersonController(IPersonContext personContext, IPersonCsvParser personCsvParser)
+    {
+        if (personContext is null)
+            throw new ArgumentNullException(null, "personContext should not be null");
+
+        if (personCsvParser is null)
+            throw new ArgumentNullException(null, "personCsvParser should not be null");
+
+        _personContext = personContext;
+        _personCsvParser = personCsvParser;
+    }
+}
