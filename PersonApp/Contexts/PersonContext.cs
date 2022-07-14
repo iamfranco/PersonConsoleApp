e3a49ab998fa4eb91f55315c64defdf1e7b9a890
iamfranco <kinfaifrancochan@gmail.com>
2022-07-14 17:05:23 +0100

    add unit test for PersonContext's People and AddPeople, create PersonContext class to pass test

---
+++ PersonApp/Contexts/PersonContext.cs
@@ -0,0 +1,16 @@
+﻿using PersonApp.Contexts;
+using PersonApp.Models;
+
+namespace PersonApp.Tests.Contexts;
+public class PersonContext : PersonContextBase
+{
+    public override List<Person> People { get; } = new List<Person>();
+
+    public override void AddPeople(List<Person> people)
+    {
+        if (people is null)
+            throw new ArgumentNullException(null, "people should not be null");
+
+        People.AddRange(people);
+    }
+}
